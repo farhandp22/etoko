b20dd273eed888d8ef7f2461f83537b072d7cd5f
farhandwiputra125 <farhandwiputra125@gmail.com>
2024-09-05 13:17:50 +0700

    implementasi models

--- main/views.py
+++ main/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from .models import Product
 
 def show_main(request):
+    data = Product.objects.all()
     context = {
         'npm' : '2306245743',
         'name': 'Farhan Dwi Putra',
-        'class': 'PBP D'
+        'class': 'PBP D', 
+        'data' : data
     }
 
     return render(request, "main.html", context)
