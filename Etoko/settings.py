1a180ec5895902f0665a783ddbfc985c08f61b84
farhandwiputra125 <farhandwiputra125@gmail.com>
2024-09-05 13:05:53 +0700

    implementasi view templates

--- Etoko/settings.py
+++ Etoko/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vzrp_jk2l1@*w^0!k(tn^ot+g4@0t^r8-2-@%bu+r2943jz108
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main'
 ]
 
 MIDDLEWARE = [
