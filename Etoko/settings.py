39066f040280780a44af831c7b7a92db215d6dc5
farhandwiputra125 <farhandwiputra125@gmail.com>
2024-09-19 10:40:42 +0700

    mengerjakan atautentikasi,session,cookies

--- Etoko/settings.py
+++ Etoko/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vzrp_jk2l1@*w^0!k(tn^ot+g4@0t^r8-2-@%bu+r2943jz108'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
+
+
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "farhan-dwi-etoko.pbp.cs.ui.ac.id"]
 
