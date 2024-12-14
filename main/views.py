d528e891efac0343c0a195f2dd1b05d2fb9d3355
farhandwiputra125 <farhandwiputra125@gmail.com>
2024-12-14 13:39:22 +0700

    add strip_tags on import

--- main/views.py
+++ main/views.py
@@ -15,6 +15,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 import json
 from django.http import JsonResponse
+from django.utils.html import strip_tags
 
 
 @login_required(login_url='/login')
